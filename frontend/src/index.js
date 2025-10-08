b0f17b8a8e43accaab06a52803adc56a7280b4e7
Logikaservice <logikaservice@gmail.com>
2025-10-08 18:11:39 +0200

    Fix: Separazione completa delle dipendenze frontend e backend

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// Se hai un file CSS per gli stili globali, importalo qui
+// import './index.css'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
