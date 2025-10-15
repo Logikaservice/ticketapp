9ddd3e79b19eb6a8ce56ce298adc60ba5c7ab789
Logikaservice <logikaservice@gmail.com>
2025-10-15 22:20:54 +0200

    Update TicketListContainer.jsx

--- frontend/src/components/TicketListContainer.jsx
+++ frontend/src/components/TicketListContainer.jsx
@@ -232,7 +232,7 @@ const TicketListContainer = ({ currentUser, tickets, users, selectedTicket, setS
       // Rimuovi l'indicatore dopo 1 secondo
       setTimeout(() => {
         setRecentCountChanges({});
-      }, 1000);
+      }, 5000);
     }
   }, [ticketCounts]);
 
