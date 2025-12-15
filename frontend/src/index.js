ed94a72adc69a86e8556a312556139ed75eb3d87
Logikaservice <logikaservice@gmail.com>
2025-12-15 20:35:25 +0100

    fix: move polyfills to separate file to solve hoisting issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,9 @@
+import './polyfills'; // MUST BE THE FIRST IMPORT
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // Se hai un file CSS per gli stili globali, importalo qui
 import './index.css';
-
-// Polyfill for Array.prototype.toSorted (ES2023) to fix compatibility issues
-if (!Array.prototype.toSorted) {
-  Array.prototype.toSorted = function (compareFn) {
-    const copy = this.slice();
-    copy.sort(compareFn);
-    return copy;
-  };
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
